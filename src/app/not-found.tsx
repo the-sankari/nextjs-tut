d61b9b910c68afc4b95192d727a15ef2fbf18bfb
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-18 23:47:50 +0300

    Resolved page not found merge conflict

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
 import React from "react";
->>>>>>> 021f3ee (Not found page initialization)
 
 export default function NotFound() {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
-  )
-=======
-      <h2>Page Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
   );
->>>>>>> 021f3ee (Not found page initialization)
 }
